9587cb9cd022a69582652d7cfec1440ad6a5e28b
dulcelene machado <dudutola@gmail.com>
2024-07-11 13:29:27 +0200

    Installed SCSS structure and initial styles

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import '../styles/main.scss';
 import { Home } from "../pages/Home";
 import { Header } from "../components/Header";
 
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import "../../styles/components/_header.scss";
 
 export const Header = () => {
   return (
