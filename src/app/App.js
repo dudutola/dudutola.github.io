f5d538ec677889e97a8278a27f9c20db2e67ed47
dulcelene machado <dudutola@gmail.com>
2024-07-11 12:34:45 +0200

    Added Home component

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "../pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route />
+        <Route path="/" element={<Home />}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,8 @@
+
+export const Home = () => {
+  return (
+    <main>
+      <h1>hello</h1>
+    </main>
+  );
+};
