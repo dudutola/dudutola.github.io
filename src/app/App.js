62cbaa23dd152b62736aaca75e2f823f5ef03ba5
dulcelene machado <dudutola@gmail.com>
2024-07-11 14:04:31 +0200

    Added 404 Error page component and routing

--- src/app/App.js
+++ src/app/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import '../styles/main.scss';
 import { Home } from "../pages/Home";
+import { Error } from "../pages/Error";
 import { Header } from "../components/Header";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/portfolio" element={<Home />}/>
+        <Route path="*" element={<Error />}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Error/index.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export const Error = () => {
+  return (
+    <div className="error">
+      <h1>404</h1>
+      <p>Oops! The page you are looking for does not exist.</p>
+      <Link to="/portfolio" className="back">Go back to Home</Link>
+    </div>
+  )
+};
