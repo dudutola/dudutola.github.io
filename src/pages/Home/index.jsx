2b7897303e6411bf66597017e99a68cb5f70a820
dulcelene machado <dudutola@gmail.com>
2024-07-12 12:43:48 +0200

    Implemented full-page scroll effect using scss

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,6 +4,15 @@ export const Home = () => {
   return (
     <main>
       <Banner />
+      <section className="two">
+        <h1>naar haus</h1>
+      </section>
+      <section className="drie">
+        <h1>naar jouw haus</h1>
+      </section>
+      <section className="vijf">
+        <h1>mijn haus</h1>
+      </section>
     </main>
   );
 };
