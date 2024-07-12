f1425530c82e2a5e5b9d921f6844e6b6a45888b6
dulcelene machado <dudutola@gmail.com>
2024-07-12 11:28:24 +0200

    Added a simple Banner component

---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,10 @@
+
+export const Banner = () => {
+  return (
+    <section className="banner">
+      <p>hallo ik ben dulce, jouw nieuwe webontwikkelaar</p>
+      <img src="" alt="dulcelene" />
+      <div className="down-arrow"></div>
+    </section>
+  )
+};
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,9 @@
-import logo from "../../logo192.png"
+import { Banner } from "../../components/Banner";
 
 export const Home = () => {
   return (
     <main>
-      <h1>hello</h1>
-      <img src={logo} alt="" />
+      <Banner />
     </main>
   );
 };
