bd6f29da133a8e156f1043786b674a32d4a268db
dulcelene machado <dudutola@gmail.com>
2024-07-12 10:18:43 +0200

    Added a simple Footer component

--- src/app/App.js
+++ src/app/App.js
@@ -4,6 +4,7 @@ import '../styles/main.scss';
 import { Home } from "../pages/Home";
 import { Error } from "../pages/Error";
 import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/portfolio" element={<Home />}/>
         <Route path="*" element={<Error />}/>
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,8 @@
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <p className="footer__text">2024</p>
+    </footer>
+  )
+};
