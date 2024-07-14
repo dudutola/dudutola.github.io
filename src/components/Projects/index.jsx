20b4a515854e63160c43fb004b945b6cc3b86095
dulcelene machado <dudutola@gmail.com>
2024-07-14 16:26:25 +0200

    Added Projects component to display projects inside cards Created projectsData.json with project data including src and figcaption

---
+++ src/components/Projects/index.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import "../../styles/components/_projects.scss";
+
+export const Projects = ({ src, figcation }) => {
+  const [isDescriptionVisible, setDescriptionVisible] = useState(false);
+
+  const handleMouseEnter = () => {
+    setDescriptionVisible(true)
+  };
+
+  const handleMouseLeave = () => {
+    setDescriptionVisible(false)
+  }
+
+  return (
+    <article
+      className="card"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      {!isDescriptionVisible && (
+        // <img src={process.env.PUBLIC_URL + image} alt="" />
+        <img src={"portfolio" + src} alt="" />
+      )}
+      {isDescriptionVisible && (
+        <p className="card__description">{figcation}</p>
+      )}
+    </article>
+  )
+};
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,41 @@
+import { useState } from "react";
 import { Banner } from "../../components/Banner";
+import { Projects } from "../../components/Projects";
+import projectsData from "../../data/projectsData.json";
 
 export const Home = () => {
+  // only show 4
+  const [projects, setProjects] = useState(projectsData.slice(0, 4));
+  const [showAll, setShowAll] = useState(false);
+
+  const handleShowAll = () => {
+    setShowAll(!showAll);
+    if (!showAll) {
+      // show all
+      setProjects(projectsData);
+    } else {
+      // show only the first 4
+      setProjects(projectsData.slice(0, 4));
+    }
+  }
+
   return (
     <main>
       <Banner />
-      <section className="two">
-        <h1>naar haus</h1>
+      <section>
+        <h1>My Projects</h1>
+        <div className="cards">
+          {projects.map((project, index) => (
+            <Projects
+              key={index}
+              src={project.src}
+              figcation={project.figcation}
+            />
+          ))}
+        </div>
+        <button onClick={handleShowAll}>
+          {showAll ? "Show Less" : "Show More"}
+        </button>
       </section>
       <section className="drie">
         <h1>naar jouw haus</h1>
