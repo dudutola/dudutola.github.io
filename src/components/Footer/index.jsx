47c5d5f30a85f6b689c0214dfcd818b30353d483
dulcelene machado <dudutola@gmail.com>
2024-07-19 11:21:11 +0200

    Updated footer adding image and content Styled footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,17 @@
 import "../../styles/components/_footer.scss";
+import { Link } from "react-router-dom";
+import github from "../../images/GitHubLogo.svg";
 
 export const Footer = () => {
   return (
     <footer className="footer">
-      <p className="footer__text">2024</p>
+      <Link to="https://github.com/dudutola">
+        <img src={github} alt="" className="footer__icon" />
+      </Link>
+      <div className="footer__content">
+        <p className="footer__content--text">&copy; 2024 Dulcelene Machado |</p>
+        <p className="footer__content--text">Tous droits réservés</p>
+      </div>
     </footer>
   )
 };
