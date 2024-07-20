9d49c40a6f0cad4b4f142dd73883add0aecbd584
dulcelene machado <dudutola@gmail.com>
2024-07-20 18:02:53 +0200

    Testing doamin name errors

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -5,7 +5,7 @@ export const Error = () => {
     <div className="error">
       <h1>404</h1>
       <p>Oops! The page you are looking for does not exist.</p>
-      <Link to="/portfolio" className="back">Go back to Home</Link>
+      <Link to="/" className="back">Go back to Home</Link>
     </div>
   )
 };
