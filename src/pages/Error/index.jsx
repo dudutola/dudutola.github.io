5ac6c9e250a71147839495142383cdfec8193d25
dulcelene machado <dudutola@gmail.com>
2024-07-22 09:52:22 +0200

    Updated and styled error page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
+import "../../styles/pages/_error.scss";
 
 export const Error = () => {
   return (
-    <div className="error">
-      <h1>404</h1>
-      <p>Oops! The page you are looking for does not exist.</p>
-      <Link to="/" className="back">Go back to Home</Link>
-    </div>
+    <main className="main-error">
+      <div className="error">
+        <h1>404</h1>
+        <p>Oops! The page you are looking for does not exist.</p>
+        <Link to="/" className="back">Go back to Home</Link>
+      </div>
+    </main>
   )
 };
